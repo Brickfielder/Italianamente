c14498f333c0e809322e6a5d4d8cc603a11bfce4
Brickfielder <mion.marco@gmail.com>
2026-01-11 20:54:20 +0000

    Fix home page code

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import Page from "./[...filename]/page";
+
 export default function Home() {
-  // Richiama la pagina standard fingendo che ci troviamo su "/home"
-  return <Page params={{ filename: ["home"] }} searchParams={{}} />;
+  return <Page params={{ filename: ["home"] }} />;
 }
