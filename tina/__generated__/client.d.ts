9f6e64e19916467333e735207bc04d5ba367cbe1
Brickfielder <mion.marco@gmail.com>
2026-01-17 12:16:26 +0000

    Fix: Correct case sensitivity for multimedia file

---
+++ tina/__generated__/client.d.ts
@@ -0,0 +1,5 @@
+import { createClient } from "tinacms/dist/client";
+import { queries } from "./types";
+export const client = createClient({ url: 'http://localhost:4001/graphql', token: 'c004ef68526f200b0838cee9fff62ec22558d0f5', queries,  });
+export default client;
+  
---
+++ tina/__generated__/config.prebuild.jsx
@@ -0,0 +1,172 @@
+// tina/config.js
+import { defineConfig } from "tinacms";
+var branch = process.env.NEXT_PUBLIC_TINA_BRANCH || "main";
+var config_default = defineConfig({
+  branch,
+  clientId: process.env.NEXT_PUBLIC_TINA_CLIENT_ID,
+  token: process.env.TINA_TOKEN,
+  build: {
+    outputFolder: "admin",
+    publicFolder: "public"
+  },
+  media: {
+    tina: {
+      mediaRoot: "uploads",
+      publicFolder: "public"
+    }
+  },
+  schema: {
+    collections: [
+      {
+        name: "page",
+        label: "Sito Web",
+        path: "content/page",
+        format: "mdx",
+        fields: [
+          {
+            type: "string",
+            name: "title",
+            label: "Titolo Pagina",
+            isTitle: true,
+            required: true
+          },
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Contenuto",
+            isBody: true
+          },
+          // TILES CONFIGURATION
+          {
+            type: "object",
+            list: true,
+            name: "tiles",
+            label: "Griglia di Card (Tiles)",
+            ui: {
+              itemProps: (item) => {
+                return { label: item.title || "Nuova Card" };
+              }
+            },
+            fields: [
+              {
+                type: "string",
+                name: "style",
+                label: "Stile Grafico",
+                options: [
+                  { label: "Standard (Bianco)", value: "standard" },
+                  { label: "Modo di Dire (Rosso)", value: "idiom" },
+                  { label: "Barzelletta (Grigio)", value: "joke" }
+                ]
+              },
+              {
+                type: "string",
+                name: "category",
+                label: "Categoria (es. Grammatica)"
+              },
+              {
+                type: "string",
+                name: "title",
+                label: "Titolo Principale"
+              },
+              {
+                type: "reference",
+                name: "postReference",
+                label: "Post di riferimento (opzionale)",
+                collections: ["post"]
+              },
+              {
+                type: "string",
+                name: "description",
+                label: "Testo Descrittivo (Opzionale)",
+                ui: { component: "textarea" }
+              },
+              {
+                type: "string",
+                name: "bulletPoints",
+                label: "Punti Elenco (Lista)",
+                list: true
+              },
+              {
+                type: "string",
+                name: "buttonText",
+                label: "Testo del Link (es. Leggi di pi\xF9)"
+              }
+            ]
+          }
+        ],
+        ui: {
+          router: ({ document }) => {
+            if (document._sys.filename === "home") {
+              return `/`;
+            }
+            if (document._sys.filename === "about") {
+              return `/about`;
+            }
+            return void 0;
+          }
+        }
+      },
+      {
+        name: "post",
+        label: "Post",
+        path: "content",
+        // Root content folder
+        match: {
+          // Include everything...
+          include: "**/*",
+          // BUT explicitly exclude the 'page' folder to avoid conflict!
+          exclude: "page/**/*"
+        },
+        format: "mdx",
+        fields: [
+          {
+            type: "string",
+            name: "title",
+            label: "Titolo",
+            required: true,
+            isTitle: true
+          },
+          {
+            type: "string",
+            name: "category",
+            label: "Categoria",
+            options: [
+              "Grammatica",
+              "Prossima vacanza",
+              "Modo di dire",
+              "Barzelletta",
+              "Ricetta",
+              "Film"
+            ],
+            required: true
+          },
+          {
+            type: "string",
+            name: "tags",
+            label: "Tag",
+            list: true
+          },
+          {
+            type: "image",
+            name: "image",
+            label: "Immagine"
+          },
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Contenuto",
+            isBody: true
+          }
+        ],
+        ui: {
+          router: ({ document }) => {
+            return `/post/${document._sys.breadcrumbs.join("/")}`;
+          }
+        }
+      }
+    ]
+  }
+});
+export {
+  config_default as default
+};
