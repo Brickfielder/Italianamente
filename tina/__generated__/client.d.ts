ed44ccb0955b5d7fd473d673ac858b02c17d4f72
Brickfielder <mion.marco@gmail.com>
2026-01-17 14:08:40 +0000

    Remove hardcoded Tina token

--- tina/__generated__/client.d.ts
+++ tina/__generated__/client.d.ts
@@ -1,5 +1,5 @@
-import { createClient } from "tinacms/dist/client";
-import { queries } from "./types";
-export const client = createClient({ url: 'http://localhost:4001/graphql', token: 'c004ef68526f200b0838cee9fff62ec22558d0f5', queries,  });
+import type { TinaClient } from "tinacms/dist/client";
+
+export declare const client: TinaClient;
 export default client;
   
--- tina/__generated__/client.js
+++ tina/__generated__/client.js
@@ -1,4 +1,11 @@
 import { createClient } from "tinacms/dist/client";
 import { queries } from "./types";
-export const client = createClient({ url: "http://localhost:4001/graphql", token: "c004ef68526f200b0838cee9fff62ec22558d0f5", queries });
+
+const token = process.env.TINA_TOKEN;
+
+export const client = createClient({
+  url: "http://localhost:4001/graphql",
+  token,
+  queries,
+});
 export default client;
