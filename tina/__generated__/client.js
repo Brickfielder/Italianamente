0a8530dd5d5d0eeb2ced0f387cfb1e649cdbee12
Brickfielder <mion.marco@gmail.com>
2026-01-18 21:13:54 +0000

    Update Tina schema artifacts

--- tina/__generated__/client.js
+++ tina/__generated__/client.js
@@ -1,4 +1,4 @@
 import { createClient } from "tinacms/dist/client";
 import { queries } from "./types";
-export const client = createClient({ cacheDir: "/workspaces/Italianamente/tina/__generated__/.cache/1768754265639", url: "https://content.tinajs.io/2.0/content/c9c0b656-f125-474d-8608-c4dba5c83116/github/main", token: "c004ef68526f200b0838cee9fff62ec22558d0f5", queries });
+export const client = createClient({ cacheDir: "/workspaces/Italianamente/tina/__generated__/.cache/1768770824062", url: "https://content.tinajs.io/2.0/content/c9c0b656-f125-474d-8608-c4dba5c83116/github/main", token: "c004ef68526f200b0838cee9fff62ec22558d0f5", queries });
 export default client;
