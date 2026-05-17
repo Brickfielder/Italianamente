498ef86b7d414af10993654994aadb7bf56521c4
Brickfielder <mion.marco@gmail.com>
2026-05-17 10:53:19 +0100

    Disable Tina post query caching

--- app/post/post-render.tsx
+++ app/post/post-render.tsx
@@ -1,10 +1,12 @@
 import { notFound } from "next/navigation";
+import { unstable_noStore as noStore } from "next/cache";
 
 import { client } from "../../tina/__generated__/client";
 import PostClient from "./post-client";
 
 export async function renderPostByPath(relativePath: string) {
   try {
+    noStore();
     const tinaProps = await client.queries.post({ relativePath });
     return <PostClient {...tinaProps} />;
   } catch (error) {
